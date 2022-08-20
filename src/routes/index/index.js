4b0894288dabd8d3d4d7cb98ac1898477900ed83
Dylan J <dylan@suhd00d.co>
2022-08-20 02:42:25 -0400

    okok

--- src/routes/index/index.js
+++ src/routes/index/index.js
@@ -1,9 +1,53 @@
 "use strict";
 
 let route = require("express").Router();
+let axios = require("axios");
 
-route.get("/", (req, res) => {
-  return res.render("index");
+route.get("/", async (req, res) => {
+  try {
+    let org_repos = await axios.get(
+      "https://api.github.com/orgs/team-tritan/repos"
+    );
+
+    let user_repos = await axios.get(
+      "https://api.github.com/users/dylanjamesdev/repos"
+    );
+
+    let all_repos = org_repos.data.concat(user_repos.data);
+    console.log(all_repos);
+
+    let repos = all_repos
+      .sort((a, b) => {
+        return b.stargazers_count - a.stargazers_count;
+      })
+      .slice(0, 6);
+
+    let data = { repos };
+
+    return res.render("index", data);
+  } catch {
+    let repos = [
+      {
+        name: "Fetch Error",
+        description:
+          "An error has occurred while fetching the data. Please try again later.",
+      },
+      {
+        name: "Fetch Error",
+        description:
+          "An error has occurred while fetching the data. Please try again later.",
+      },
+      {
+        name: "Fetch Error",
+        description:
+          "An error has occurred while fetching the data. Please try again later.",
+      },
+    ];
+
+    let data = { repos };
+
+    return res.render("index", data);
+  }
 });
 
 module.exports = route;
