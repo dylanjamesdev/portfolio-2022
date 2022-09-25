01711acdb85f567ed7a9ea35834c41bcedcb8153
root <root@dylan.colo-fl.myinfra.lol>
2022-09-25 05:16:54 +0000

    added redirs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,32 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://github.com/dylanjamesdev',
+        permanent: true,
+      },
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/http',
+        permanent: true,
+      },
+      {
+        source: '/webmaster',
+        destination: '/rants/webmaster',
+        permanent: true,
+      },
+      {
+        source: '/words',
+        destination: '/rants/words',
+        permanent: true,
+      },
+
+    ]
+  },
+
 };
 
 module.exports = nextConfig;
