20618c1e5f907e1b03cb28c7ef2ed6167439a234
Dylan J <dylan@suhd00d.co>
2022-09-25 01:04:47 -0400

    api ported to next

---
+++ pages/api/v1/fetchRepos.ts
@@ -0,0 +1,48 @@
+"use strict";
+
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+export default async function getRepos(
+  _req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    let org_repos = await axios.get(
+      "https://api.github.com/orgs/team-tritan/repos"
+    );
+
+    let user_repos = await axios.get(
+      "https://api.github.com/users/dylanjamesdev/repos"
+    );
+
+    let all_repos = org_repos.data.concat(user_repos.data);
+
+    let repos = all_repos
+      .sort((a, b) => b.size - a.size)
+      .filter((i) => !i.fork)
+      .slice(0, 9);
+
+    return res.json(repos);
+  } catch {
+    let repos = [
+      {
+        name: "Fetch Error",
+        description:
+          "A temporary api error has occurred while fetching the data.",
+      },
+      {
+        name: "Fetch Error",
+        description:
+          "A temporary api error has occurred while fetching the data.",
+      },
+      {
+        name: "Fetch Error",
+        description:
+          "A temporary api error has occurred while fetching the data.",
+      },
+    ];
+
+    return res.json(repos);
+  }
+}
