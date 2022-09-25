11b6ec4021982774e3d0e511abeefbbab1291c95
Dylan J <dylan@suhd00d.co>
2022-09-25 01:19:11 -0400

    remove css from <head> bc framework

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,6 @@ export default function Document() {
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Dylan James - Software Engineer</title>
-      <link rel="stylesheet" href="/css/index.css" />
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
