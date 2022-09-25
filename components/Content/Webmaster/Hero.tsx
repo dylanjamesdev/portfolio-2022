ec23dd43e95ea697362ff14b38e9769b35a070fe
Dylan J <dylan@suhd00d.co>
2022-09-25 01:04:20 -0400

    webmaster page

---
+++ components/Content/Webmaster/Hero.tsx
@@ -0,0 +1,20 @@
+"use strict";
+
+export default function WebmasterHero() {
+  return (
+    <section className="landing">
+      <div className="row vertical-center" id="landing">
+        <div className="col-lg-8 text-left pb-5">
+          <h1 className="display-1">
+            I'm a fucking{"  "}
+            <span style={{ color: "rgb(71, 71, 172)" }}>webmaster</span>.
+          </h1>
+          <p style={{ color: "white" }}>
+            This is a rant which I strongly believe everyone should read. If you
+            disagree I don't give a shit.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
