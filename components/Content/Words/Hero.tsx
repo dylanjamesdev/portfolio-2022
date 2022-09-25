bec1679acb91a4f31b59f1ff13923ed54d6c75e8
Dylan J <dylan@suhd00d.co>
2022-09-25 01:04:28 -0400

    words page

---
+++ components/Content/Words/Hero.tsx
@@ -0,0 +1,21 @@
+"use strict";
+
+export default function WordsHero() {
+  return (
+    <section className="landing">
+      <div className="row vertical-center" id="landing">
+        <div className="col-lg-8 text-left pb-5">
+          <h1 className="display-1">
+            Your
+            <span style={{ color: "rgb(71, 71, 172)" }}>words</span> are
+            powerful.
+          </h1>
+          <p style={{ color: "white" }}>
+            It seems like people have forgotten the foundation of what the
+            internet is.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ components/Content/Words/Text.tsx
@@ -0,0 +1,82 @@
+"use strict";
+
+export default function WordsText() {
+  return (
+    <section className="animate__animated animate__fadeInUp" id="contact">
+      <div className="text-left">
+        <div className="pt-3">
+          <h1>This is a web page.</h1>
+          <p>There's not much here.</p>
+          <p>Just words.</p>
+          <p>
+            <strong>And you're reading them.</strong>
+          </p>
+          <p>
+            We've become obsessed with fancy designs, responsive layouts, and
+            scripts that do magical things.
+          </p>
+          <p>
+            <strong>
+              But the most powerful tool on the web is still words.
+            </strong>
+          </p>
+          <p>
+            I wrote these words, and you're reading them:
+            <em>that's</em> magical.
+          </p>
+          <p>
+            I'm in a little city in Florida; you're probably somewhere else. I
+            wrote this early in the morning, August 25th, 2022; you're reading
+            it at a different time. I wrote this on my laptop; you could be
+            reading this on your phone, a tablet or a desktop.
+          </p>
+          <p>
+            You and I have been able to connect because<em> I wrote this </em>
+            and <em>you're reading it.</em> <strong>That's the web</strong>.
+            Despite our different locations, devices, and time-zones we can
+            connect <em>here, </em>on a simple HTML page.
+          </p>
+          <p>
+            I wrote this in a text editor. It's 6&nbsp;kB. I didn't need a
+            Content Management System, a graphic designer, or a software
+            developer. There's not much code on this page at all: just simple
+            markup for paragraphs, hierarchy, and emphasis.
+          </p>
+          <p>
+            <strong>You are still reading.</strong>
+          </p>
+          <p>
+            Think about all the things you could communicate with a simple page
+            like this. If you're a businessperson, you could sell something. If
+            you're a teacher, you could teach something. If you're an artist,
+            you could show something you've made.
+            <strong>And if your words are good, people will read them.</strong>
+          </p>
+          <p>
+            If you're a web designer, I challenge you to think about the words
+            first. Instead of starting with a style guide or a Photoshop mockup,
+            <em>start with words on a page</em>.
+          </p>
+          <p>
+            What do you have to say? If you don't know, there's not much use in
+            adding all that other cruft.
+            <strong>Just start with one page</strong>, with a single focus.
+            Write it and publish it, and then <em>iterate on that</em>. Every
+            time you're about to add something, ask yourself: does this help me
+            communicate better? Will that additional styling, image, or
+            hyperlink give my audience more understanding? If the answer is
+            "no," don't add it.
+          </p>
+          <p>
+            <strong>At its heart, web design should be about words</strong>.
+            Words don't come <em>after</em> the design is done. Words are the
+            beginning, the core, the focus.
+          </p>
+          <p>Start with words.</p>
+        </div>
+      </div>
+      <br />
+      <br />
+    </section>
+  );
+}
